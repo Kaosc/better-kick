67ab34d076d2df615e7bb573fff5e66516018417
Kaosc <themelihbt@gmail.com>
2024-04-02 08:15:43 +0300

    create repo

---
+++ public/content.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+	setTimeout(() => {
+		var elementToRemove = document.getElementsByClassName("section-title")[1]?.parentElement
+		if (elementToRemove) {
+			elementToRemove.remove()
+		}
+		var showMoreButton = document.getElementsByClassName("show-action-btn-label")[0]
+		if (showMoreButton) {
+			showMoreButton.click()
+		}
+	}, 500)
+}
